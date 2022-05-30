f15bb098df0ee0e14f03e33eba72478e975a3a09
minwoo <alsdndia789@ajou.ac.kr>
2022-05-30 12:57:09 +0900

    [HOT_FIX] login response Dto

--- src/main/java/shoppingmall/core/web/dto/TokenDto.java
+++ src/main/java/shoppingmall/core/web/dto/TokenDto.java
@@ -6,10 +6,12 @@ import shoppingmall.core.domain.member.Member;
 @Getter
 public class TokenDto {
 
+    private Long id;
     private String account;
     private String token;
 
     public TokenDto(Member entity, String token) {
+        this.id = entity.getId();
         this.account = entity.getAccount();
         this.token = token;
     }
